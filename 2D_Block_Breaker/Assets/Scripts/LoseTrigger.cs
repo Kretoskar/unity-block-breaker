6a9d7700533f72a4c3e32960e5dfcc661c157912
Oskar Kogut <kogutoskar123@gmail.com>
2018-07-27 13:42:01 +0200

    Added GameOverTrigger

---
+++ 2D_Block_Breaker/Assets/Scripts/LoseTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseTrigger : MonoBehaviour {
+
+    [SerializeField]
+    GameObject gameOverUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameOverUI.SetActive(true);
+    }
+}
