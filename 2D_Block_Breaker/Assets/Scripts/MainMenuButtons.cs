0746940776e93af49eddc252a4bfcf9a56c17bce
Oskar Kogut <kogutoskar123@gmail.com>
2018-07-27 12:38:00 +0200

    MainMenu buttons work now

---
+++ 2D_Block_Breaker/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void OnButtonPlayClicked ()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int sceneToLoad = currentSceneIndex + 1;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void OnButtonQuitClicked()
+    {
+        Application.Quit();
+    }
+}
