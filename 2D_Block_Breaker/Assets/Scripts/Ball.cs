ee832a7d456b158c0171f8d5670f99e81ee98767
Oskar Kogut <kogutoskar123@gmail.com>
2018-07-27 14:32:21 +0200

    Added onClick ball push

--- 2D_Block_Breaker/Assets/Scripts/Ball.cs
+++ 2D_Block_Breaker/Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +9,15 @@ public class Ball : MonoBehaviour {
     [SerializeField]
     private Paddle mPaddle;
 
+    [SerializeField]
+    private float mXPush = 0f;
+
+    [SerializeField]
+    private float mYPush = 15f;
+
     //state
     private Vector2 mPaddleToBallVector;
+    private bool mHasStarted = false;
 
 	// Use this for initialization
 	void Start ()
@@ -20,7 +28,25 @@ public class Ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if (!mHasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(mXPush, mYPush);
+            mHasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(mPaddle.transform.position.x, mPaddle.transform.position.y);
         transform.position = paddlePos + mPaddleToBallVector;
-	}
+    }
 }
