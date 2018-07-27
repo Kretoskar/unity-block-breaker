c0cb72ca08aab62df42ff801bd73503a28679795
Oskar Kogut <kogutoskar123@gmail.com>
2018-07-27 13:33:43 +0200

    GameOver buttons work now

---
+++ 2D_Block_Breaker/Assets/Scripts/GameOverButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtons : MonoBehaviour {
+
+	public void OnButtonTryAgainCLicked ()
+    {
+        int sceneToLoad = 1;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void OnButtonQuitClicked ()
+    {
+        Application.Quit();
+    }
+}
